dec89932a56d44811221384065d739d43961e3d6
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-10 22:33:39 +0530

    final

--- cypress/integration/tests/test.spec.js
+++ cypress/integration/tests/test.spec.js
@@ -3,17 +3,7 @@ describe('App', () => {
       // Visit the application url
       cy.visit('http://localhost:8080')
     })
-  
-    it('logs in a user', () => {
-      cy.get('#login-btn').click()
-      cy.get('#current-user').should('contain', 'Current user:rohan, isAuthenticated: Yes')
-    })
-  
-    it('logs out a user', () => {
-      cy.get('#login-btn').click()
-      cy.get('#signout').click()
-      cy.get('#current-user').should('contain', 'Current user:, isAuthenticated: No')
-    })
+
   
     it('adds an item', () => {
       const item = 'apple'
